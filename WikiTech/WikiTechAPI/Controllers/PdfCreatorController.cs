6e1d8ccd8579f1c9a1502db6563616ce6b06412b
Marco Pancini <marco-loto@hotmail.com>
2021-05-25 11:20:22 +0200

    viktorloan

---
+++ WikiTech/WikiTechAPI/Controllers/PdfCreatorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wkhtmltopdf.NetCore;
+
+namespace WikiTechAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfCreatorController : ControllerBase
+    {
+        readonly IGeneratePdf generatePdf;
+        public PdfCreatorController(IGeneratePdf generatePdf)
+        {
+            this.generatePdf = generatePdf;
+        }
+    }
+}
---
+++ WikiTech/WikiTechAPI/Models/PDF_Generator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WikiTechAPI.Models
+{
+    public class PDF_Generator
+    {
+        public class Bill
+        {
+            public string Name { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string Gender { get; set; }
+        }
+    }
+}
