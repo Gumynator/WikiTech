0164d8185baea19be56aba007467c7fcbfa8781c
blitzkill97 <l.habegger97@gmail.com>
2021-05-30 14:25:36 +0200

    error 404

---
+++ WikiTech/WikiTechWebApp/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WikiTechWebApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+
+            switch (statusCode)
+            {
+
+                case 404:
+                    ViewBag.ErrorMessage = "Erreur 404, la ressource n'est pas trouvée";
+                    break;
+
+            }
+
+            return View("NotFound");
+        }
+    }
+}
